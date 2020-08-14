dd81345c18a16d5189b3e90d01ea96e6dacea2a5
Huckemer <57465307+Huckemer@users.noreply.github.com>
2020-08-14 18:18:31 -0500

    initialized with content

---
+++ src/Components/Resume.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+
+class Resume extends Component {
+
+  getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  render() {
+
+    if (this.props.data) {
+      var education = this.props.data.education.map(function (education) {
+        return <div key={education.school}><h3><a href={education.link}>{education.school}</a></h3>
+          <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+          <ul>
+            {education.description.map(item => (
+              <span style={{ "list-style-type": "circle" }}><li key={item}>{item}</li></span>
+            ))}
+          </ul></div>
+      })
+
+      var work = this.props.data.work.map(function (work) {
+        return <div key={work.company}><h3><a href={work.link} target="_blank">{work.company}</a></h3>
+          <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+          <ul>
+            {work.description.map(item => (
+              <span style={{ "list-style-type": "circle" }}><li key={item}>{item}</li></span>
+            ))}
+          </ul>
+        </div>
+      })
+
+      var skills = this.props.data.skills.map((skills) => {
+        var className = 'bar-expand ' + skills.name.toLowerCase();
+        return (
+          <li key={skills.name}>
+            <span style={{ width: skills.level, backgroundColor: this.getRandomColor() }} className={className}></span><em>{skills.name}</em>
+          </li>
+        )
+      })
+
+      var tools = this.props.data.tools.map((tools) => {
+        var className = 'bar-expand ' + tools.name.toLowerCase();
+        return (
+          <li key={tools.name}>
+            <span style={{ width: tools.level, backgroundColor: this.getRandomColor() }} className={className}></span><em>{tools.name}</em>
+          </li>
+        )
+      })
+    }
+
+    return (
+      <section id="resume">
+
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1><span>Education</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">
+                {education}
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="row work">
+
+          <div className="three columns header-col">
+            <h1><span>Work</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+            {work}
+          </div>
+        </div>
+
+
+        <div className="row work">
+
+          <div className="three columns header-col">
+            <h1><span>Languages</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+
+            <div className="bars">
+              <ul className="skills">
+                {skills}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="row skill">
+
+          <div className="three columns header-col">
+            <h1><span>Tools</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+
+            <div className="bars">
+              <ul className="tools">
+                {tools}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Resume;
